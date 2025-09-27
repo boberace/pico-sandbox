87542b5f01eed926ab0486a194d19aa8e11d2171
boberace <boberace@github.com>
2025-09-26 20:06:10 -0400

    scratching sine

--- sine_wave/sine_wave.c
+++ sine_wave/sine_wave.c
@@ -8,46 +8,59 @@
 #include <math.h>
 
 #if PICO_ON_DEVICE
+
 #include "hardware/clocks.h"
 #include "hardware/structs/clocks.h"
+
 #endif
 
 #include "pico/stdlib.h"
+
+#if USE_AUDIO_I2S
+
 #include "pico/audio_i2s.h"
 
 #if PICO_ON_DEVICE
 #include "pico/binary_info.h"
-bi_decl(bi_3pins_with_names(PICO_AUDIO_I2S_DATA_PIN, "I2S DIN",
-                           PICO_AUDIO_I2S_CLOCK_PIN_BASE, "I2S BCK",
-                           PICO_AUDIO_I2S_CLOCK_PIN_BASE + 1, "I2S LRCK"));
+bi_decl(bi_3pins_with_names(PICO_AUDIO_I2S_DATA_PIN, "I2S DIN", PICO_AUDIO_I2S_CLOCK_PIN_BASE, "I2S BCK", PICO_AUDIO_I2S_CLOCK_PIN_BASE+1, "I2S LRCK"));
+#endif
+
+#elif USE_AUDIO_PWM
+// #define PICO_AUDIO_PWM_L_PIN 2
+// #define PICO_AUDIO_PWM_R_PIN 3
+// #define PICO_AUDIO_PWM_MONO_PIN PICO_AUDIO_PWM_L_PIN
+#include "pico/audio_pwm.h"
+#elif USE_AUDIO_SPDIF
+#include "pico/audio_spdif.h"
 #endif
 
 #define SINE_WAVE_TABLE_LEN 2048
 #define SAMPLES_PER_BUFFER 256
-#define SAMPLE_RATE 24000
 
 static int16_t sine_wave_table[SINE_WAVE_TABLE_LEN];
 
-const struct audio_format *output_format;
+struct audio_buffer_pool *init_audio() {
 
-struct audio_buffer_pool *init_audio(void) {
-
-    /* Request a PCM S16 stereo format at 24 kHz for I2S */
     static audio_format_t audio_format = {
             .format = AUDIO_BUFFER_FORMAT_PCM_S16,
-            .sample_freq = SAMPLE_RATE,
-            .channel_count = 2,
+#if USE_AUDIO_SPDIF
+            .sample_freq = 44100,
+#else
+            .sample_freq = 24000,
+#endif
+        .channel_count = 2,
     };
 
-    struct audio_buffer_format producer_format;
-    producer_format.format = &audio_format;
-    /* bytes per frame (channels * 2 bytes per int16 sample) */
-    producer_format.sample_stride = sizeof(int16_t) * audio_format.channel_count;
+    static struct audio_buffer_format producer_format = {
+            .format = &audio_format,
+        .sample_stride = 4
+    };
 
     struct audio_buffer_pool *producer_pool = audio_new_producer_pool(&producer_format, 3,
-                                                                      SAMPLES_PER_BUFFER);
+                                                                      SAMPLES_PER_BUFFER); // todo correct size
     bool __unused ok;
-
+    const struct audio_format *output_format;
+#if USE_AUDIO_I2S
     struct audio_i2s_config config = {
             .data_pin = PICO_AUDIO_I2S_DATA_PIN,
             .clock_pin_base = PICO_AUDIO_I2S_CLOCK_PIN_BASE,
@@ -57,97 +70,95 @@ struct audio_buffer_pool *init_audio(void) {
 
     output_format = audio_i2s_setup(&audio_format, &config);
     if (!output_format) {
-        panic("PicoAudio: Unable to open I2S audio device.\n");
-    } else {
-        printf("I2S: sample rate %u, channels %u\n", output_format->sample_freq,
-               output_format->channel_count);
+        panic("PicoAudio: Unable to open audio device.\n");
     }
 
-
-    
     ok = audio_i2s_connect(producer_pool);
     assert(ok);
     audio_i2s_set_enabled(true);
+#elif USE_AUDIO_PWM
 
+    output_format = audio_pwm_setup(&audio_format, -1, &default_mono_channel_config);
+    
+    if (!output_format) {
+        panic("PicoAudio: Unable to open audio device.\n");
+    }
+    
+    ok = audio_pwm_default_connect(producer_pool, false);    
+    assert(ok);    
+    audio_pwm_set_enabled(true);
+        
+#elif USE_AUDIO_SPDIF
+    output_format = audio_spdif_setup(&audio_format, &audio_spdif_default_config);
+    if (!output_format) {
+        panic("PicoAudio: Unable to open audio device.\n");
+    }
+    //ok = audio_spdif_connect(producer_pool);
+    ok = audio_spdif_connect(producer_pool);
+    assert(ok);
+    audio_spdif_set_enabled(true);
+#endif
     return producer_pool;
 }
 
-/* compute step for desired frequency (safe 64-bit intermediate) */
-static uint32_t compute_step_for_freq(uint32_t freq_hz, uint32_t sample_rate_hz) {
-    return (uint32_t)((uint64_t)freq_hz * (uint64_t)SINE_WAVE_TABLE_LEN * 65536ULL / sample_rate_hz);
-}
+int main() {
+#if PICO_ON_DEVICE
+#if USE_AUDIO_PWM
+    set_sys_clock_48mhz();
+#endif
+#endif
 
-/* inverse: compute frequency (Hz) from step and sample rate */
-static double compute_freq_from_step(uint32_t step_q16, uint32_t sample_rate_hz) {
-    return ((double)sample_rate_hz * (double)step_q16 / 65536.0) / (double)SINE_WAVE_TABLE_LEN;
-}
-
-int main(void) {
     stdio_init_all();
-    sleep_ms(2000); // wait for usb serial to connect
-    printf("\nSine wave generator (I2S stereo - independent L/R)\n");
 
     for (int i = 0; i < SINE_WAVE_TABLE_LEN; i++) {
-        sine_wave_table[i] = 32767 * cosf(i * 2 * (float)(M_PI / SINE_WAVE_TABLE_LEN));
-    } 
+        sine_wave_table[i] = 32767 * cosf(i * 2 * (float) (M_PI / SINE_WAVE_TABLE_LEN));
+    }
 
     struct audio_buffer_pool *ap = init_audio();
-    uint32_t sample_rate = output_format ? output_format->sample_freq : SAMPLE_RATE;
-
-    /* Use the same frequency on both channels, but offset the right channel by 90 degrees */
-    uint32_t desired_hz = 220; /* frequency for both channels */
-    uint32_t step_left  = compute_step_for_freq(desired_hz, sample_rate);
-    uint32_t step_right = step_left; /* same frequency */
-
-    uint32_t pos_left = 0;
-    uint32_t pos_max  = 0x10000 * SINE_WAVE_TABLE_LEN;
-/* 90 degree phase offset for right channel = 1/4 of the cycle */
-    uint32_t pos_right = (pos_left + pos_max / 4) % pos_max;
-
+    uint32_t step = 0x200000;
+    uint32_t pos = 0;
+    uint32_t pos_max = 0x10000 * SINE_WAVE_TABLE_LEN;
     uint vol = 128;
-
-    const uint32_t channels = output_format ? output_format->channel_count : 2;
-
     while (true) {
+#if USE_AUDIO_PWM
+        enum audio_correction_mode m = audio_pwm_get_correction_mode();
+#endif
         int c = getchar_timeout_us(0);
         if (c >= 0) {
             if (c == '-' && vol) vol -= 4;
             if ((c == '=' || c == '+') && vol < 255) vol += 4;
-            /* keyboard controls to change pitch independently:
-               '[' / ']' : decrease/increase LEFT step by one table-index/sample
-               'a' / 'd' : decrease/increase RIGHT step by one table-index/sample
-               'q' : quit
-            */
-            if (c == '[' && step_left > 0x10000) step_left -= 0x10000;
-            if (c == ']' && step_left < (SINE_WAVE_TABLE_LEN / 16) * 0x20000) step_left += 0x10000;
-            if (c == 'a' && step_right > 0x10000) step_right -= 0x10000;
-            if (c == 'd' && step_right < (SINE_WAVE_TABLE_LEN / 16) * 0x20000) step_right += 0x10000;
+            if (c == '[' && step > 0x10000) step -= 0x10000;
+            if (c == ']' && step < (SINE_WAVE_TABLE_LEN / 16) * 0x20000) step += 0x10000;
             if (c == 'q') break;
-
-            double freqL = compute_freq_from_step(step_left, sample_rate);
-            double freqR = compute_freq_from_step(step_right, sample_rate);
-            printf("vol=%d  stepL=%d stepR=%d  freqL=%.2fHz freqR=%.2fHz      \r",
-                   vol, step_left >> 16, step_right >> 16, freqL, freqR);
+#if USE_AUDIO_PWM
+            if (c == 'c') {
+                bool done = false;
+                while (!done) {
+                    if (m == none) m = fixed_dither;
+                    else if (m == fixed_dither) m = dither;
+                    else if (m == dither) m = noise_shaped_dither;
+                    else if (m == noise_shaped_dither) m = none;
+                    done = audio_pwm_set_correction_mode(m);
+                }
+            }
+            printf("vol = %d, step = %d mode = %d      \r", vol, step >>16, m);
+#else
+            printf("vol = %d, step = %d      \r", vol, step >> 16);
+#endif
         }
-
-        // give_audio_buffer(ap, buffer);
         struct audio_buffer *buffer = take_audio_buffer(ap, true);
         int16_t *samples = (int16_t *) buffer->buffer->bytes;
         // interleaved stereo: L,R,L,R,... duplicate same sine on both channels
         for (uint i = 0; i < buffer->max_sample_count; i++) {
-            int16_t sl = (vol * sine_wave_table[pos_left >> 16u]) >> 8u;
-            int16_t sr = (vol * sine_wave_table[pos_right >> 16u]) >> 8u;
-            samples[i * 2] = sl;
-            samples[i * 2 + 1] = sr;
-            pos_left  += step_left;
-            pos_right += step_right;
-            if (pos_left  >= pos_max) pos_left  -= pos_max;
-            if (pos_right >= pos_max) pos_right -= pos_max;
+            int16_t s = (vol * sine_wave_table[pos >> 16u]) >> 8u;
+            samples[i * 2] = s;
+            samples[i * 2 + 1] = s;
+            pos += step;
+            if (pos >= pos_max) pos -= pos_max;
         }
         buffer->sample_count = buffer->max_sample_count;
         give_audio_buffer(ap, buffer);
     }
-
     puts("\n");
     return 0;
 }
