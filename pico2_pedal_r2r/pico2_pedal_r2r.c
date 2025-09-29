fb7a00bd1f2a98fb11f3516bbf4a67c75ddaf8d3
boberace <boberace@github.com>
2025-09-29 19:42:46 -0400

    created pico2 pedal using r2r DAC and internal ADC. scratching on other r2r

--- pico2_pedal_r2r/pico2_pedal_r2r.c
+++ pico2_pedal_r2r/pico2_pedal_r2r.c
@@ -5,10 +5,13 @@
 #include "hardware/pio.h"
 #include "hardware/dma.h"
 #include "hardware/clocks.h"
+#include "hardware/adc.h"
 #include "blink.pio.h"
 #include "r2r.pio.h"
 
 #define system_frequency clock_get_hz(clk_sys)
+
+#define CAPTURE_CHANNEL 0 // ADC0 is GPIO26
 
 #define R2R_BITS 10
 #define PIN_R2R_BASE 0 
@@ -21,15 +24,15 @@ uint sm_r2r = 0;
 volatile uint16_t fun_wave[NUM_WAV_SAMPLES];
 volatile uint16_t * p_fun_wave = &fun_wave[0];
 
-int dma_chan_wave_bytes;
-int dma_chan_wave_loop;
+int dma_chan_dac_data, dma_chan_dac_loop, dma_chan_adc_data;
 
 void blink_program_init(PIO pio, uint sm, uint offset, uint pin);
 void blink_pin_forever(PIO pio, uint sm, uint offset, uint pin, uint freq);
 void setup_waves();
 void r2r_program_init(PIO pio, uint sm, uint offset, uint base_pin, uint num_pins, float freq);
 void setup_r2r(float freq);
-void setup_dma_wave();
+void setup_dma_adc();
+void setup_dma_dac();
 
 int main()
 {
@@ -38,7 +41,8 @@ int main()
     setup_waves();  
     setup_r2r(20000.0);
 
-    setup_dma_wave();
+    setup_dma_adc();
+    setup_dma_dac();
 
     // PIO Blinking example
     PIO pio = pio0;
@@ -46,9 +50,11 @@ int main()
     printf("Loaded program at %d\n", offset);    
 
     blink_pin_forever(pio, 0, offset, 25, 2);
+    uint count = 0;
 
     while (true) {
-        printf("Hello, world!\n");
+        printf("%d, system freq %d\n", count, system_frequency);
+        count++;
         sleep_ms(1000);
     }
 }
@@ -77,8 +83,8 @@ void blink_pin_forever(PIO pio, uint sm, uint offset, uint pin, uint freq)
 void setup_waves() 
 { // =ROUND(63*(SIN(A1*2*PI()/256)/2+0.5),0)
     for (int i = 0; i < NUM_WAV_SAMPLES; i++) {
-        fun_wave[i] = (uint16_t)round(R2R_MAX * (sin(i * 2 * M_PI / NUM_WAV_SAMPLES) / 2 + 0.5));
-        // fun_wave[i] = i;
+        // fun_wave[i] = (uint16_t)round(R2R_MAX * (sin(i * 2 * M_PI / NUM_WAV_SAMPLES) / 2 + 0.5));
+        fun_wave[i] = 1;
     }
 }
 
@@ -86,7 +92,7 @@ void r2r_program_init(PIO pio, uint sm, uint offset, uint base_pin, uint num_pin
 {
     pio_sm_config c = r2r_program_get_default_config(offset);
     sm_config_set_out_pins(&c, base_pin, num_pins); 
-    sm_config_set_out_shift(&c, true, true, R2R_BITS); 
+    sm_config_set_out_shift(&c, true, true, 10); 
     for(int i = 0; i < num_pins; i++) {
         pio_gpio_init(pio, base_pin + i);
     }
@@ -105,26 +111,66 @@ void setup_r2r(float freq)
 }
 
 
-void setup_dma_wave()
+void setup_dma_adc(){
+
+    adc_gpio_init(26 + CAPTURE_CHANNEL);
+    adc_init();
+    adc_select_input(CAPTURE_CHANNEL);
+    adc_fifo_setup(
+        true,    // Write each completed conversion to the sample FIFO
+        true,    // Enable DMA data request (DREQ)
+        1,       // DREQ (and IRQ) asserted when at least 1 sample present
+        false,   // Not using ERR bit
+        false    // Do not Shift each sample to 8 bits when pushing to FIFO
+    );
+
+    adc_set_clkdiv(10); // 50khz
+
+
+    sleep_ms(1000);
+    // Set up the DMA to start transferring data as soon as it appears in FIFO
+    dma_chan_adc_data = dma_claim_unused_channel(true);
+    dma_channel_config cfg = dma_channel_get_default_config(dma_chan_adc_data);
+
+    // Reading from constant address, writing to incrementing byte addresses
+    channel_config_set_transfer_data_size(&cfg, DMA_SIZE_16);
+    channel_config_set_read_increment(&cfg, false);
+    channel_config_set_write_increment(&cfg, true);
+
+    // Pace transfers based on availability of ADC samples
+    channel_config_set_dreq(&cfg, DREQ_ADC);
+
+    dma_channel_configure(dma_chan_adc_data, &cfg,
+        fun_wave,    // dst
+        &adc_hw->fifo,  // src
+        NUM_WAV_SAMPLES,  // transfer count
+        true            // start immediately
+    );
+
+    printf("Starting capture\n");
+    adc_run(true);
+}
+
+void setup_dma_dac()
 {
 
-    dma_chan_wave_bytes = dma_claim_unused_channel(true); 
-    dma_chan_wave_loop = dma_claim_unused_channel(true);
+    dma_chan_dac_data = dma_claim_unused_channel(true); 
+    dma_chan_dac_loop = dma_claim_unused_channel(true);
 
-    dma_channel_config c_wave = dma_channel_get_default_config(dma_chan_wave_bytes);
-    dma_channel_config c_loop = dma_channel_get_default_config(dma_chan_wave_loop);
+    dma_channel_config c_data = dma_channel_get_default_config(dma_chan_dac_data);
+    dma_channel_config c_loop = dma_channel_get_default_config(dma_chan_dac_loop);
 
     uint pio_r2r_dreq = pio_get_dreq(pio_r2r, sm_r2r, true);
       
-    channel_config_set_read_increment(&c_wave, true);
-    channel_config_set_write_increment(&c_wave, false);
-    channel_config_set_dreq(&c_wave, pio_r2r_dreq);
-    channel_config_set_chain_to(&c_wave, dma_chan_wave_loop);
-    channel_config_set_transfer_data_size(&c_wave, DMA_SIZE_16);
-    channel_config_set_irq_quiet(&c_wave, false);
+    channel_config_set_read_increment(&c_data, true);
+    channel_config_set_write_increment(&c_data, false);
+    channel_config_set_dreq(&c_data, pio_r2r_dreq);
+    channel_config_set_chain_to(&c_data, dma_chan_dac_loop);
+    channel_config_set_transfer_data_size(&c_data, DMA_SIZE_16);
+    channel_config_set_irq_quiet(&c_data, false);
     dma_channel_configure(
-        dma_chan_wave_bytes, 
-        &c_wave, 
+        dma_chan_dac_data, 
+        &c_data, 
         &pio1_hw->txf[sm_r2r], 
         NULL, // loop channel fills this in
         NUM_WAV_SAMPLES, 
@@ -133,13 +179,13 @@ void setup_dma_wave()
 
     channel_config_set_read_increment(&c_loop, false);
     channel_config_set_write_increment(&c_loop, false);
-    channel_config_set_chain_to(&c_loop, dma_chan_wave_bytes);
+    channel_config_set_chain_to(&c_loop, dma_chan_dac_data);
     channel_config_set_transfer_data_size(&c_loop, DMA_SIZE_32);
     channel_config_set_irq_quiet(&c_loop, false);
     dma_channel_configure(
-        dma_chan_wave_loop, 
+        dma_chan_dac_loop, 
         &c_loop, 
-        &dma_hw->ch[dma_chan_wave_bytes].read_addr, 
+        &dma_hw->ch[dma_chan_dac_data].read_addr, 
         &p_fun_wave,
         1, 
         true
